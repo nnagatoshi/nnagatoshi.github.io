19208535725a4138959bfec6985bd2042aa31ae0
Nicole Nagatoshi <54603725+nnagatoshi@users.noreply.github.com>
2021-10-26 17:21:07 -1000

    Add files via upload

---
+++ punchcard.js
@@ -0,0 +1,49 @@
+var card = [
+    {
+        "title": "New Punch card",
+        "image": "./images/StampCardNon.jpg"
+    },
+    {
+        "title": "One Punch",
+        "image": "./images/StampCardOne.jpg"
+    },
+    {
+        "title": "Two Punch",
+        "image": "./images/StampCardTwo.jpg"
+    },
+    {
+        "title": "Three Punch",
+        "image": "./images/StampCardThree.jpg"
+    },
+    {
+        "title": "Four Punch",
+        "image": "./images/StampCardFour.jpg"
+    },
+    {
+        "title": "Five Punch",
+        "image": "./images/StampCardFive.jpg"
+    },
+    {
+        "title": "Six Punch",
+        "image": "./images/StampCardSix.jpg"
+    },
+    {
+        "title": "Seven Punch",
+        "image": "./images/StampCardSeven.jpg"
+    },
+    {
+        "title": "Eight Punch",
+        "image": "./images/StampCardEight.jpg"
+    },
+    {
+        "title": "Nine Punch",
+        "image": "./images/StampCardNine.jpg"
+    },
+    {
+        "title": "FREE",
+        "image": "./images/StampCardFull.jpg"
+    },
+];
+if (typeof module != 'undefined') {
+    module.exports.card = card;
+    }
